0d62dd43c984eb9b096335938d8d35847ddf3334
Jun Aoki <jaoki@apache.org>
2015-09-18 17:55:23 -0700

    fixed

--- ambari-web/app/models/configs/objects/service_config_property.js
+++ ambari-web/app/models/configs/objects/service_config_property.js
@@ -406,7 +406,7 @@ App.ServiceConfigProperty = Em.Object.extend({
             }
           } else {
             if (!validator.isValidDir(value)) {
-              this.set('errorMessage', 'Must be a slash or drive at the start');
+              this.set('errorMessage', 'Must be a slash or drive at the start, or must not contain a space');
               isError = true;
             }
           }
