2e14327579ea63441cc1f8785b1cfa0f87572d0b
Jun Aoki <jaoki@apache.org>
2015-10-12 17:45:47 -0700

    use or

--- dev-support/test-patch.sh
+++ dev-support/test-patch.sh
@@ -775,9 +775,7 @@ findModules () {
   CHANGED_MODULES=""
   for module in $(cat $TMP_MODULES | sort | uniq); do
     $GREP "<packaging>pom</packaging>" $module/pom.xml > /dev/null
-    if [ "$?" != 0 ]; then
-      CHANGED_MODULES="$CHANGED_MODULES $module"
-    elif [ "$module" == "ambari-web" ]; then # ambari-web is packing = pom but has unit tests
+    if [ "$?" != 0 || "$module" == "ambari-web" ]; then # ambari-web is packing = pom but has unit tests
       CHANGED_MODULES="$CHANGED_MODULES $module"
     fi
   done
